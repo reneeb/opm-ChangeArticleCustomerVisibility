573e71f9e2ba842cc603f6d7be5f2fa8482a8fee
reneeb <info@perl-services.de>
2022-01-25 09:46:32 +0100

    add feature: change article dynamic fields

--- Custom/Kernel/Modules/AgentTicketChangeArticleCustomerVisibility.pm
+++ Custom/Kernel/Modules/AgentTicketChangeArticleCustomerVisibility.pm
@@ -1,5 +1,5 @@
 # --
-# Copyright (C) 2019 Perl-Services.de, http://perl-services.de
+# Copyright (C) 2019 - 2022 Perl-Services.de, https://perl-services.de
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -12,6 +12,9 @@ use strict;
 use warnings;
  
 our $ObjectManagerDisabled = 1;
+
+use Kernel::System::VariableCheck qw(:all);
+use Kernel::Language qw(Translatable);
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -26,11 +29,15 @@ sub new {
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $ConfigObject  = $Kernel::OM->Get('Kernel::Config');
     my $LayoutObject  = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
     my $ParamObject   = $Kernel::OM->Get('Kernel::System::Web::Request');
     my $TicketObject  = $Kernel::OM->Get('Kernel::System::Ticket');
     my $ArticleObject = $Kernel::OM->Get('Kernel::System::Ticket::Article');
     my $DBObject      = $Kernel::OM->Get('Kernel::System::DB');
+
+    my $DynamicFieldObject        = $Kernel::OM->Get('Kernel::System::DynamicField');
+    my $DynamicFieldBackendObject = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
 
     my @Params = qw(IsVisibleForCustomer ArticleID TicketID);
     my %GetParam;
@@ -42,12 +49,153 @@ sub Run {
     $GetParam{IsVisibleForCustomer} ||= 0;
     $GetParam{IsVisibleForCustomer}   = 1 if $GetParam{IsVisibleForCustomer};
 
-    if ( $Self->{Subaction} eq 'ChangeVisibility' && $GetParam{ArticleID} ) {
+    # get ACL restrictions
+    my %PossibleActions = ( 1 => $Self->{Action} );
+
+    my $ACL = $TicketObject->TicketAcl(
+        Data          => \%PossibleActions,
+        Action        => $Self->{Action},
+        TicketID      => $Self->{TicketID},
+        ReturnType    => 'Action',
+        ReturnSubType => '-',
+        UserID        => $Self->{UserID},
+    );
+
+    my %AclAction = $TicketObject->TicketAclActionData();
+
+    # check if ACL restrictions exist
+    if ( $ACL || IsHashRefWithData( \%AclAction ) ) {
+
+        my %AclActionLookup = reverse %AclAction;
+
+        # show error screen if ACL prohibits this action
+        if ( !$AclActionLookup{ $Self->{Action} } ) {
+            return $LayoutObject->NoPermission( WithHeader => 'yes' );
+        }
+    }
+
+    my $Config = $ConfigObject->Get('Ticket::Frontend::' . $Self->{Action});
+
+    # get the dynamic fields for this screen
+    my $DynamicField = $DynamicFieldObject->DynamicFieldListGet(
+        Valid       => 1,
+        ObjectType  => [ 'Article' ],
+        FieldFilter => $Config->{DynamicField} || {},
+    );
+
+    my %DynamicFieldData;
+    my %DynamicFieldError;
+
+    DYNAMICFIELD:
+    for my $DynamicFieldConfig ( @{$DynamicField} ) {
+
+        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+
+        my $Name = $DynamicFieldConfig->{Name};
+
+        # extract the dynamic field value from the web request
+        $DynamicFieldData{Value}->{ $Name } =
+            $DynamicFieldBackendObject->EditFieldValueGet(
+            DynamicFieldConfig => $DynamicFieldConfig,
+            ParamObject        => $ParamObject,
+            LayoutObject       => $LayoutObject,
+        );
+ 
+
+        my $IsACLReducible = $DynamicFieldBackendObject->HasBehavior(
+            DynamicFieldConfig => $DynamicFieldConfig,
+            Behavior           => 'IsACLReducible',
+        );
+
+        $DynamicFieldData{ACLReducible}->{$Name} = $IsACLReducible;
+
+        my $PossibleValues = $DynamicFieldBackendObject->PossibleValuesGet(
+            DynamicFieldConfig => $DynamicFieldConfig,
+        );
+
+        $DynamicFieldData{PossibleValues}->{$Name} = $PossibleValues;
+        if ( $IsACLReducible ) {
+
+            # convert possible values key => value to key => key for ACLs using a Hash slice
+            my %AclData = %{$PossibleValues};
+            @AclData{ keys %AclData } = keys %AclData;
+
+            # set possible values filter from ACLs
+            my $ACL = $TicketObject->TicketAcl(
+                %GetParam,
+                Action        => $Self->{Action},
+                TicketID      => $Self->{TicketID},
+                QueueID       => $Self->{QueueID},
+                ReturnType    => 'Ticket',
+                ReturnSubType => 'DynamicField_' . $Name,
+                Data          => \%AclData,
+                UserID        => $Self->{UserID},
+            );
+
+            if ($ACL) {
+                my %Filter = $TicketObject->TicketAclData();
+
+                # convert Filer key => key back to key => value using map
+                %{$PossibleValues} = map { $_ => $PossibleValues->{$_} } keys %Filter;
+            }
+
+            my $DataValues = $DynamicFieldBackendObject->BuildSelectionDataGet(
+                DynamicFieldConfig => $DynamicFieldConfig,
+                PossibleValues     => $PossibleValues,
+                Value              => $DynamicFieldData{Value}->{$Name},
+            ) || $PossibleValues;
+
+            $DynamicFieldData{PossibleValues}->{$Name} = $DataValues;
+        }
+
+        if ( $Self->{Subaction} && $Self->{Subaction} ne 'AJAXUpdate' ) {
+            my $ValidationResult = $DynamicFieldBackendObject->EditFieldValueValidate(
+                DynamicFieldConfig   => $DynamicFieldConfig,
+                PossibleValuesFilter => $DynamicFieldData{PossibleValues}->{$Name},
+                ParamObject          => $ParamObject,
+                Mandatory            => $Config->{DynamicField}->{$Name} == 2,
+            );
+
+            if ( IsHashRefWithData( $ValidationResult ) && $ValidationResult->{ServerError} ) {
+                $DynamicFieldError{$Name} = $ValidationResult;
+            }
+        }
+    }
+
+    # convert dynamic field values into a structure for ACLs
+    my %DynamicFieldACLParameters;
+    DYNAMICFIELDITEM:
+    for my $Name ( sort keys %{ $DynamicFieldData{Value} || {} } ) {
+        next DYNAMICFIELDITEM if !$Name;
+        next DYNAMICFIELDITEM if !defined $DynamicFieldData{Value}->{$Name};
+
+        $DynamicFieldACLParameters{ 'DynamicField_' . $Name } = $DynamicFieldData{Value}->{$Name};
+    }
+
+    $GetParam{DynamicField} = \%DynamicFieldACLParameters;
+
+    if ( $Self->{Subaction} eq 'ChangeVisibility' && $GetParam{ArticleID} && !%DynamicFieldError ) {
         my $SQL     = 'UPDATE article SET is_visible_for_customer = ? WHERE id = ?';
         my $Success = $DBObject->Do(
             SQL  => $SQL,
             Bind => [ \$GetParam{IsVisibleForCustomer}, \$GetParam{ArticleID} ],
         );
+
+        DYNAMICFIELD:
+        for my $DynamicFieldConfig ( @{$DynamicField} ) {
+            next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+
+            my $Name = $DynamicFieldConfig->{Name};
+
+            # set the value
+            my $Success = $DynamicFieldBackendObject->ValueSet(
+                DynamicFieldConfig => $DynamicFieldConfig,
+                ObjectID           => $GetParam{ArticleID},
+                Value              => $DynamicFieldData{Value}->{$Name},
+                UserID             => $Self->{UserID},
+            );
+Kernel::LOG( "$Name -> SUCCESS : " . $Success );
+        }
 
         if ( $Success ) {
             $TicketObject->_TicketCacheClear( TicketID => $GetParam{TicketID} );
@@ -57,6 +205,42 @@ sub Run {
             );
         }
     }
+    elsif ( $Self->{Subaction} eq 'AJAXUpdate' ) {
+        # update Dynamic Fields Possible Values via AJAX
+        my @DynamicFieldAJAX;
+
+        # cycle through the activated Dynamic Fields for this screen
+        DYNAMICFIELD:
+        for my $DynamicFieldConfig ( @{$DynamicField} ) {
+            next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+
+            my $Name = $DynamicFieldConfig->{Name};
+
+            next DYNAMICFIELD if !$DynamicFieldData{ACLReducible}->{$Name};
+
+            push(
+                @DynamicFieldAJAX,
+                {
+                    Name        => 'DynamicField_' . $Name,
+                    Data        => $DynamicFieldData{PossibleValues}->{$Name},
+                    SelectedID  => $DynamicFieldData{Value}->{$Name},
+                    Translation => $DynamicFieldConfig->{Config}->{TranslatableValues} || 0,
+                    Max         => 100,
+                }
+            );
+        }
+
+        my $JSON = $LayoutObject->BuildSelectionJSON(
+            \@DynamicFieldAJAX,
+        );
+
+        return $LayoutObject->Attachment(
+            ContentType => 'application/json; charset=' . $LayoutObject->{Charset},
+            Content     => $JSON,
+            Type        => 'inline',
+            NoCache     => 1,
+        );
+    }
 
     my $Backend = $ArticleObject->BackendForArticle(
         TicketID  => $GetParam{TicketID},
@@ -64,9 +248,53 @@ sub Run {
     );
 
     my %Article = $Backend->ArticleGet(
-        TicketID  => $GetParam{TicketID},
-        ArticleID => $GetParam{ArticleID},
+        TicketID      => $GetParam{TicketID},
+        ArticleID     => $GetParam{ArticleID},
+        DynamicFields => 1,
     );
+
+    # Dynamic fields
+    DYNAMICFIELD:
+    for my $DynamicFieldConfig ( @{$DynamicField} ) {
+        next DYNAMICFIELD if !IsHashRefWithData($DynamicFieldConfig);
+
+        my $Name = $DynamicFieldConfig->{Name};
+
+        my $Class = '';
+        if ( $Config->{DynamicField}->{$Name} == 2 ) {
+            $Class = 'Validate_Required';
+        }
+
+        my $Value = $Article{'DynamicField_' . $Name};
+
+        # Get field html.
+        my $DynamicFieldHTML = $DynamicFieldBackendObject->EditFieldRender(
+            DynamicFieldConfig   => $DynamicFieldConfig,
+            PossibleValuesFilter => $DynamicFieldData{PossibleValues}->{$Name},
+            ServerError          => $DynamicFieldError{$Name}->{ServerError} || '',
+            ErrorMessage         => $DynamicFieldError{$Name}->{ErrorMessage} || '',
+            Mandatory            => ( $Class eq 'Validate_Required' ) ? 1 : 0,
+            Class                => $Class,
+            LayoutObject         => $LayoutObject,
+            ParamObject          => $ParamObject,
+            AJAXUpdate           => 1,
+            Value                => $Value,
+            UpdatableFields      => [
+                grep {
+                    $DynamicFieldData{ACLReducible}->{$_}
+                } keys %{ $DynamicFieldData{ACLReducible} || {} }
+            ],
+        );
+
+        $LayoutObject->Block(
+            Name => 'DynamicField',
+            Data => {
+                Name  => $DynamicFieldConfig->{Name},
+                Label => $DynamicFieldHTML->{Label},
+                Field => $DynamicFieldHTML->{Field},
+            },
+        );
+    }
 
     my $Output = $LayoutObject->Header( Type => 'Small' );
     $Output .= $LayoutObject->Output(
---
+++ Custom/Kernel/Output/HTML/ArticleAction/AgentTicketChangeArticleCustomerVisibility.pm
@@ -0,0 +1,94 @@
+# --
+# Copyright (C) 2022 Perl-Services.de, https://perl-services.de
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (GPL). If you
+# did not receive this file, see https://www.gnu.org/licenses/gpl-3.0.txt.
+# --
+
+package Kernel::Output::HTML::ArticleAction::AgentTicketChangeArticleCustomerVisibility;
+
+use strict;
+use warnings;
+
+use Kernel::Language qw(Translatable);
+
+our @ObjectDependencies = (
+    'Kernel::Config',
+    'Kernel::System::Log',
+);
+
+sub new {
+    my ( $Type, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {};
+    bless( $Self, $Type );
+
+    return $Self;
+}
+
+# optional AclActionLookup
+sub CheckAccess {
+    my ( $Self, %Param ) = @_;
+
+    # Check needed stuff.
+    for my $Needed (qw(Ticket Article ChannelName UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    # check basic conditions
+    if ( $Param{ChannelName} eq 'Internal' ) {
+        return;
+    }
+
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    # check if module is registered
+    return if !$ConfigObject->Get('Frontend::Module')->{AgentTicketChangeArticleCustomerVisibility};
+
+    # check Acl
+    return if !$Param{AclActionLookup}->{AgentTicketChangeArticleCustomerVisibility};
+
+    return 1;
+}
+
+sub GetConfig {
+    my ( $Self, %Param ) = @_;
+
+    # Check needed stuff.
+    for my $Needed (qw(Ticket Article UserID)) {
+        if ( !$Param{$Needed} ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need $Needed!",
+            );
+            return;
+        }
+    }
+
+    my $Link = "Action=AgentTicketChangeArticleCustomerVisibility&" .
+        "TicketID=$Param{Ticket}->{TicketID}&" .
+        "Test=1&" .
+        "ArticleID=$Param{Article}->{ArticleID}";
+
+    my $Description = Translatable('Change customer visibility');
+
+    my %MenuItem = (
+        ItemType    => 'Link',
+        Description => $Description,
+        Name        => $Description,
+        Class       => 'AsPopup PopupType_TicketAction',
+        Link        => $Link,
+    );
+
+    return ( \%MenuItem );
+}
+
+1;
